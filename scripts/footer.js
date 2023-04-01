ff5751b4c7e10be4d6541d8d990dcdd3b26af302
unknown <kumarrohitsingh824@gmail.com>
2023-04-01 22:54:52 +0530

    day-4

---
+++ scripts/footer.js
@@ -0,0 +1,51 @@
+function footer(){
+    return `  <hr>
+    <div id="footer_title">
+         <img src="./images/thetimemachine.jpeg" alt="">
+          <h2>TheTimeTracking</h2>
+    </div>
+          <div id="footer_main">
+            <div>
+                <h3>Applications</h3>
+                <h4><i class="fa-solid fa-desktop"></i>Desktop</h4>
+                <h4><i class="fa-brands fa-apple"></i>iOS</h4>
+                <h4><i class="fa-brands fa-android"></i>Android</h4>
+            </div>
+            <div>
+                <h3>Browser Extensions</h3>
+                <h4><i class="fa-brands fa-chrome"></i>Chrome</h4>
+                <h4><i class="fa-brands fa-firefox"></i>Firefox</h4>
+                <h4><i class="fa-brands fa-opera"></i>Opera</h4>
+                <h4><i class="fa-brands fa-edge"></i>Edge</h4>
+                <h4><i class="fa-brands fa-safari"></i>Safari</h4>
+            </div>
+             <div>
+                <h3>Resources</h3>
+                <h4><i class="fa-solid fa-dna"></i>Solutions</h4>
+                <h4><i class="fa-solid fa-network-wired"></i>Integrations</h4>
+                <h4><i class="fa-solid fa-comments"></i>Help</h4>
+                <h4><i class="fa-brands fa-times"></i>Time Trackers Comparison</h4>
+                <h4><i class="fa-brands fa-blogger"></i>Blog</h4>
+            </div>
+            <div>
+                <h3>About</h3>
+                <h4>About TMetric</h4>
+                <h4>Terms of Service</h4>
+                <h4>Privacy Policy</h4>
+                <h4>Cookies Policy</h4>
+            </div>
+          </div>
+          <hr>
+          <div id='footer_down'>
+            <div>
+                <h3>Copyright © Devart 20223</h3>
+            </div>
+            <div id="footer_down_icon">
+                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+          </div>`
+}
+export default footer
